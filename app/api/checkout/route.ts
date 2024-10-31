abfd765678a119c92ef252a71c7d7d388cfbbe70
Luis Ribeiro <luisleandro.wd@gmail.com>
2024-10-31 19:52:07 +0000

    fixed imports

--- app/api/checkout/route.ts
+++ app/api/checkout/route.ts
@@ -1,22 +1,33 @@
 // app/api/checkout/route.ts
-import { type NextRequest, NextResponse } from "next/server";
-const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+import { type NextRequest, NextResponse } from 'next/server';
+import Stripe, { type Stripe as StripeType } from 'stripe';
+if (!process.env.STRIPE_SECRET_KEY) {
+  throw new Error('STRIPE_SECRET_KEY is not defined');
+}
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
+  apiVersion: '2024-09-30.acacia',
+});
 
 export async function POST(request: NextRequest) {
   const res = await request.json();
   const stripe_product_ids = res.stripe_product_ids;
   try {
-    let line_items = [];
-    let mode = "payment";
+    const line_items = [];
+    let mode: StripeType.Checkout.SessionCreateParams.Mode = 'payment';
     for (const stripe_product_id of stripe_product_ids) {
       const product = await stripe.products.retrieve(stripe_product_id);
+      if (typeof product.default_price !== 'string') {
+        throw new Error(
+          `Invalid default price for product ${stripe_product_id}`
+        );
+      }
       const price = await stripe.prices.retrieve(product.default_price);
       line_items.push({
         price: price.id,
         quantity: 1,
       });
       // If any items are recurring
-      if (price.type === "recurring") mode = "subscription";
+      if (price.type === 'recurring') mode = 'subscription';
     }
     const session = await stripe.checkout.sessions.create({
       line_items,
